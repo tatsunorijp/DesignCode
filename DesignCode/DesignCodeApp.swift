a8c71558db61b779cb4bc4ba3b82c0fdd49c14d8
Wellington Tatsunori Asahide <tatsunoripai@gmail.com>
2022-03-28 20:16:24 -0400

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Wellington Tatsunori Asahide on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
